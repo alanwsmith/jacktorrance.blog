2ca128daec12c9a518d7a0655c8ce4d98c774576
Alan W. Smith <alan@alanwsmith.com>
2021-12-30 16:00:53 -0500

    Basic nav in place

--- pages/[page].js
+++ pages/[page].js
@@ -1,21 +1,12 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
 
 import WorkPage from '../components/WorkPage'
 
 export default function Home() {
-  // const [pageNum, setPageNum] = useState(1)
-  // const router = useRouter()
-
-  // useEffect(() => {
-  //   if (!router.isReady) return
-  //   if (router.query['page']) {
-  //     const page = router.query['page']
-  //     setPageNum(router.query['page'])
-  //   }
-  // }, [router.isReady, router.query])
+  const router = useRouter()
+  const { page } = router.query
 
   return (
     <div>
@@ -24,11 +15,9 @@ export default function Home() {
         <meta name="description" content="The writing of Jack Torrance" />
       </Head>
 
-      <WorkPage page="1" />
-
       <div className="flex flex-col h-screen">
         <div className="flex-grow" id="container"></div>
-        <div></div>
+        <WorkPage page={page} />
         <div className="text-xs text-right">This is the footer</div>
       </div>
     </div>
