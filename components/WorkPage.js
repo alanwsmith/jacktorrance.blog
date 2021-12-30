905d9ba1b32cf8cd7d0f3b30014892c4f7b36c9b
Alan W. Smith <alan@alanwsmith.com>
2021-12-30 16:07:29 -0500

    Added WorkPage component

---
+++ components/WorkPage.js
@@ -0,0 +1,74 @@
+import Link from 'next/link'
+
+const pages = [
+  '',
+  `all work and no play makes jack a dull boy
+all work and no play makes jack a dull boy
+all work and no play makes jack a dull boy
+all work and no play makes jack a dull boy`,
+  'ALL work and no Play makes jack a dull boy',
+  'allworkandnoplaymakesjackadullboy',
+  `all work
+and no play
+makes jack
+a dull boy`,
+]
+
+export default function WorkPage({ page }) {
+  var pageNav
+  const baseNum = parseInt(page, 10)
+  if (page === '1') {
+    pageNav = (
+      <div>
+        <Link href="/2">
+          <a>Page 2 </a>
+        </Link>
+      </div>
+    )
+  } else if (baseNum === 2) {
+    pageNav = (
+      <>
+        <div>
+          <Link href="/">
+            <a>Page {baseNum - 1}</a>
+          </Link>{' '}
+          -{' '}
+          <Link href={`/${baseNum + 1}`}>
+            <a>Page {baseNum + 1}</a>
+          </Link>
+        </div>
+      </>
+    )
+  } else if (baseNum < pages.length - 1) {
+    pageNav = (
+      <>
+        <div>
+          <Link href={`/${baseNum - 1}`}>
+            <a>Page {baseNum - 1}</a>
+          </Link>{' '}
+          -{' '}
+          <Link href={`/${baseNum + 1}`}>
+            <a>Page {baseNum + 1}</a>
+          </Link>
+        </div>
+      </>
+    )
+  } else {
+    pageNav = (
+      <>
+        <div>
+          <Link href={`/${baseNum - 1}`}>
+            <a>Page {baseNum - 1}</a>
+          </Link>{' '}
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <pre>{pages[page]}</pre>
+      {pageNav}
+    </>
+  )
+}
