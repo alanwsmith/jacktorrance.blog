a666ca123c93c2c1f489ee7cf654f9857a303138
Alan W. Smith <alan@alanwsmith.com>
2022-01-03 13:18:45 -0500

    Added hardcoded 404 page that shows page 404

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import WorkPage from '../components/WorkPage'
+
+export default function Home() {
+  const router = useRouter()
+  const { page } = router.query
+
+  return (
+    <div>
+      <Head>
+        <title>Autobiography Of Jack Torrance</title>
+        <meta name="description" content="The writing of Jack Torrance" />
+      </Head>
+      <WorkPage page="404" />
+    </div>
+  )
+}
